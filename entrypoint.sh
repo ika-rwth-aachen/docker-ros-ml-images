de1cde7c2e2b456bf55c66561ffe44b8762f5b21
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2024-03-14 21:59:46 +0100

    check if new user exists in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,19 +9,21 @@ if [[ $DOCKER_UID && $DOCKER_GID ]]; then
     if ! getent group $DOCKER_GID > /dev/null 2>&1; then
         groupadd -g $DOCKER_GID $DOCKER_USER
     fi
-    useradd -s /bin/bash \
-            -u $DOCKER_UID \
-            -g $DOCKER_GID \
-            --create-home \
-            --home-dir /home/$DOCKER_USER \
-            --groups sudo,video \
-            --password "$(openssl passwd -1 $DOCKER_USER)" \
-            $DOCKER_USER && \
-            touch /home/$DOCKER_USER/.sudo_as_admin_successful
-    cp /root/.bashrc /home/$DOCKER_USER
-    ln -s $WORKSPACE /home/$DOCKER_USER/ws
-    chown -R $DOCKER_UID:$DOCKER_GID $WORKSPACE
-    chown -R $DOCKER_UID:$DOCKER_GID /home/$DOCKER_USER
+    if ! getent passwd $DOCKER_UID > /dev/null 2>&1; then
+        useradd -s /bin/bash \
+                -u $DOCKER_UID \
+                -g $DOCKER_GID \
+                --create-home \
+                --home-dir /home/$DOCKER_USER \
+                --groups sudo,video \
+                --password "$(openssl passwd -1 $DOCKER_USER)" \
+                $DOCKER_USER && \
+                touch /home/$DOCKER_USER/.sudo_as_admin_successful
+        cp /root/.bashrc /home/$DOCKER_USER
+        ln -s $WORKSPACE /home/$DOCKER_USER/ws
+        chown -R $DOCKER_UID:$DOCKER_GID $WORKSPACE
+        chown -R $DOCKER_UID:$DOCKER_GID /home/$DOCKER_USER
+    fi
     [[ $(pwd) == "$WORKSPACE" ]] && cd /home/$DOCKER_USER/ws
     exec gosu $DOCKER_USER "$@"
 else
