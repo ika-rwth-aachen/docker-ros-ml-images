676a5a49374f53b1980a27445e3aa17ecd38241b
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2024-06-04 23:44:59 +0200

    fix 2digit version numbers in version_information.sh

--- .version_information.sh
+++ .version_information.sh
@@ -5,9 +5,9 @@ eval $(cat /etc/os-release | grep ^VERSION=)
 
 ARCH=$(uname -m)
 
-CUDA_VERSION=$(dpkg -l 2> /dev/null | grep -E "cuda-cudart-[0-9]" | awk '{ print $3 }')
-CUDNN_VERSION=$(dpkg -l 2> /dev/null | grep -E "libcudnn[0-9] " | awk '{ print $3 }')
-TENSORRT_VERSION=$(dpkg -l 2> /dev/null | grep -E "libnvinfer[0-9] " | awk '{ print $3 }')
+CUDA_VERSION=$(dpkg -l 2> /dev/null | grep -E "cuda-cudart-[0-9]." | awk '{ print $3 }')
+CUDNN_VERSION=$(dpkg -l 2> /dev/null | grep -E "libcudnn[0-9]. " | awk '{ print $3 }')
+TENSORRT_VERSION=$(dpkg -l 2> /dev/null | grep -E "libnvinfer[0-9]. " | awk '{ print $3 }')
 
 PYTHON_VERSION=$(python --version | awk '{print $2}')
 TF_PIP_VERSION=$(python -c "exec(\"try:\n  import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'; import tensorflow as tf; print(tf.__version__);\n\rexcept ImportError:\n  pass\")")
