95908fe141dfdad12b39019e3232de125d5937a3
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-08-26 15:22:18 +0200

    improve generation of image version table

--- utils/export_image_versions.py
+++ utils/export_image_versions.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import subprocess
 import tqdm
+import csv
 
 VERSION_GETTER_COMMANDS = {
     "Arch": "dpkg --print-architecture",
@@ -20,14 +21,17 @@ VERSION_GETTER_COMMANDS = {
     "TensorFlow": "export TF_CPP_MIN_LOG_LEVEL='1' && python -c 'exec(\\\"try:\\n  import os; import tensorflow as tf; print(tf.__version__);\\n\\rexcept ImportError:\\n  pass\\\")' | cut -d+ -f1 | cut -d- -f1",
 }
 
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Prints tool versions for docker-ros-ml-images")
     parser.add_argument("images", help="File listing Docker images")
+    parser.add_argument("--arch", help="Architecture to filter images", choices=["amd64", "arm64"], required=True)
     return parser.parse_args()
 
-def get_image_list(file_path):
+def get_image_list(file_path, arch):
     with open(file_path, "r") as file:
-        return [line.strip() for line in file if line.strip()]
+        return [line.strip() for line in file if line.strip().endswith(f"-{arch}")]
 
 def get_tool_versions(image_name):
     result = {}
@@ -45,15 +49,14 @@ def get_tool_versions(image_name):
             result[tool] = f"Error: {e.output.decode('utf-8').strip()}"
     return result
 
-def print_markdown_table(data):
-    headers = ["Tag"] + list(VERSION_GETTER_COMMANDS.keys())
-    header_line = " | ".join(headers)
-    separator_line = " | ".join(["---"] * len(headers))
-    print(header_line)
-    print(separator_line)
-    for image, versions in data.items():
-        row = [image] + [versions.get(tool, "-") for tool in VERSION_GETTER_COMMANDS]
-        print(" | ".join(row))
+def export_to_csv(data, file_path):
+    with open(file_path, 'w') as csvfile:
+        field_names = ["Tag"] + list(VERSION_GETTER_COMMANDS.keys())
+        writer = csv.DictWriter(csvfile, fieldnames = field_names)
+        writer.writeheader()
+        for image in data:
+            short_image = image.split("/")[-1]
+            writer.writerow({"Tag": short_image, **data[image]})
 
 def main():
     args = parse_arguments()
@@ -61,14 +64,13 @@ def main():
     if not os.path.exists(image_list_file):
         print(f"Error: File {image_list_file} not found.")
         return
-    images = get_image_list(image_list_file)
+    images = get_image_list(image_list_file, args.arch)
     all_versions = {}
     with tqdm.tqdm(images, desc="Getting info") as pbar:
         for image in pbar:
             pbar.set_postfix(image=image)
             all_versions[image] = get_tool_versions(image)
-    print("")
-    print_markdown_table(all_versions)
+    export_to_csv(all_versions, os.path.join(SCRIPT_PATH, f"image_versions-{args.arch}.csv"))
 
 if __name__ == "__main__":
     main()
