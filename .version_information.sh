0cf27f45361022f6aa6df944492b2d7041f2b23f
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2024-12-04 12:55:33 +0100

    improve version information and add tegra release version

--- .version_information.sh
+++ .version_information.sh
@@ -5,6 +5,8 @@ eval $(cat /etc/os-release | grep ^VERSION=)
 
 ARCH=$(uname -m)
 
+[[ -f /etc/nv_tegra_release ]] && TEGRA_RELEASE=$(cat /etc/nv_tegra_release | grep -o "R[0-9]* (release)" | awk '{print $1}').$(cat /etc/nv_tegra_release | grep -o "REVISION: [0-9]*\.[0-9]*" | awk '{print $2}')
+
 if [[ -z "$RMW_IMPLEMENTATION" && $ROS_DISTRO != "noetic" ]]; then
   if ros2 pkg list | grep -q "rmw_fastrtps_cpp"; then
     export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
@@ -33,18 +35,23 @@ cat << EOF
 === CONTAINER INFORMATION ======================================================
 Architecture: $ARCH
 Ubuntu: $VERSION
-Python: $PYTHON_VERSION
-ROS: $ROS_DISTRO
-RMW: $RMW_IMPLEMENTATION
-CMake: $CMAKE_VERSION
-CUDA: $CUDA_VERSION
-cuDNN: $CUDNN_VERSION
-TensorRT: $TENSORRT_VERSION
-Triton Client: $TRITON_VERSION
-TensorFlow: $TF_PIP_VERSION
-PyTorch: $PT_PIP_VERSION
-Available GPUs: $NUM_GPUS
-$GPU_INFOS
+EOF
+
+[[ -n "$TEGRA_RELEASE" ]] && echo "Tegra Release: $TEGRA_RELEASE"
+[[ -n "$PYTHON_VERSION" ]] && echo "Python: $PYTHON_VERSION"
+[[ -n "$ROS_DISTRO" ]] && echo "ROS: $ROS_DISTRO"
+[[ -n "$RMW_IMPLEMENTATION" ]] && echo "RMW: $RMW_IMPLEMENTATION"
+[[ -n "$CMAKE_VERSION" ]] && echo "CMake: $CMAKE_VERSION"
+[[ -n "$CUDA_VERSION" ]] && echo "CUDA: $CUDA_VERSION"
+[[ -n "$CUDNN_VERSION" ]] && echo "cuDNN: $CUDNN_VERSION"
+[[ -n "$TENSORRT_VERSION" ]] && echo "TensorRT: $TENSORRT_VERSION"
+[[ -n "$TRITON_VERSION" ]] && echo "Triton Client: $TRITON_VERSION"
+[[ -n "$TF_PIP_VERSION" ]] && echo "TensorFlow: $TF_PIP_VERSION"
+[[ -n "$PT_PIP_VERSION" ]] && echo "PyTorch: $PT_PIP_VERSION"
+[[ -n "$NUM_GPUS" ]] && echo "Available GPUs: $NUM_GPUS"
+[[ -n "$GPU_INFOS" ]] && echo "$GPU_INFOS"
+
+cat << EOF
 ================================================================================
 
 EOF
