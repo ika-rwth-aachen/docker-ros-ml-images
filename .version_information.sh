8122c4bc94778642e9e74bfce1af4039943540b8
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2024-12-04 14:16:04 +0100

    beautify version information

--- .version_information.sh
+++ .version_information.sh
@@ -5,7 +5,7 @@ eval $(cat /etc/os-release | grep ^VERSION=)
 
 ARCH=$(uname -m)
 
-[[ -f /etc/nv_tegra_release ]] && TEGRA_RELEASE=$(cat /etc/nv_tegra_release | grep -o "R[0-9]* (release)" | awk '{print $1}').$(cat /etc/nv_tegra_release | grep -o "REVISION: [0-9]*\.[0-9]*" | awk '{print $2}')
+[[ -f /etc/nv_tegra_release ]] && JETSON_LINUX_VERSION=$(cat /etc/nv_tegra_release | grep -o "R[0-9]* (release)" | awk '{print $1}').$(cat /etc/nv_tegra_release | grep -o "REVISION: [0-9]*\.[0-9]*" | awk '{print $2}')
 
 if [[ -z "$RMW_IMPLEMENTATION" && $ROS_DISTRO != "noetic" ]]; then
   if ros2 pkg list | grep -q "rmw_fastrtps_cpp"; then
@@ -24,34 +24,31 @@ PT_PIP_VERSION=$(python -c "exec(\"try:\n  import torch; print(torch.__version__
 CMAKE_VERSION=$(cmake --version  | grep version | awk '{print $3}')
 if [[ $(command -v nvidia-smi) ]]; then
   NUM_GPUS=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
-  GPU_INFOS=$(nvidia-smi --query-gpu=name,driver_version,utilization.gpu,utilization.memory,memory.used,memory.total --format=csv | column -t -s, | sed 's/^/  /')
 else
   NUM_GPUS="0"
-  GPU_INFOS=""
 fi
 
 # print information
 cat << EOF
-=== CONTAINER INFORMATION ======================================================
-Architecture: $ARCH
-Ubuntu: $VERSION
+╔══════════════════════════════════════════════════ CONTAINER INFORMATION ═════╗
 EOF
-
-[[ -n "$TEGRA_RELEASE" ]] && echo "Tegra Release: $TEGRA_RELEASE"
-[[ -n "$PYTHON_VERSION" ]] && echo "Python: $PYTHON_VERSION"
-[[ -n "$ROS_DISTRO" ]] && echo "ROS: $ROS_DISTRO"
-[[ -n "$RMW_IMPLEMENTATION" ]] && echo "RMW: $RMW_IMPLEMENTATION"
-[[ -n "$CMAKE_VERSION" ]] && echo "CMake: $CMAKE_VERSION"
-[[ -n "$CUDA_VERSION" ]] && echo "CUDA: $CUDA_VERSION"
-[[ -n "$CUDNN_VERSION" ]] && echo "cuDNN: $CUDNN_VERSION"
-[[ -n "$TENSORRT_VERSION" ]] && echo "TensorRT: $TENSORRT_VERSION"
-[[ -n "$TRITON_VERSION" ]] && echo "Triton Client: $TRITON_VERSION"
-[[ -n "$TF_PIP_VERSION" ]] && echo "TensorFlow: $TF_PIP_VERSION"
-[[ -n "$PT_PIP_VERSION" ]] && echo "PyTorch: $PT_PIP_VERSION"
-[[ -n "$NUM_GPUS" ]] && echo "Available GPUs: $NUM_GPUS"
-[[ -n "$GPU_INFOS" ]] && echo "$GPU_INFOS"
+printf "║ %12s | %-61s ║\n" "Architecture" "$ARCH"
+printf "║ %12s | %-61s ║\n" "Ubuntu" "$VERSION"
+[[ -n "$JETSON_LINUX_VERSION" ]] && printf "║ %12s | %-61s ║\n" "Jetson Linux" "$JETSON_LINUX_VERSION"
+[[ $(getent passwd $DOCKER_USER) ]] && printf "║ %12s | %-61s ║\n" "User:PW" "$DOCKER_USER:$DOCKER_USER"
+[[ -n "$PYTHON_VERSION" ]] && printf "║ %12s | %-61s ║\n" "Python" "$PYTHON_VERSION"
+[[ -n "$ROS_DISTRO" ]] && printf "║ %12s | %-61s ║\n" "ROS" "$ROS_DISTRO"
+[[ -n "$RMW_IMPLEMENTATION" ]] && printf "║ %12s | %-61s ║\n" "RMW" "$RMW_IMPLEMENTATION"
+[[ -n "$CMAKE_VERSION" ]] && printf "║ %12s | %-61s ║\n" "CMake" "$CMAKE_VERSION"
+[[ -n "$CUDA_VERSION" ]] && printf "║ %12s | %-61s ║\n" "CUDA" "$CUDA_VERSION"
+[[ -n "$CUDNN_VERSION" ]] && printf "║ %12s | %-61s ║\n" "cuDNN" "$CUDNN_VERSION"
+[[ -n "$TENSORRT_VERSION" ]] && printf "║ %12s | %-61s ║\n" "TensorRT" "$TENSORRT_VERSION"
+[[ -n "$TRITON_VERSION" ]] && printf "║ %12s | %-61s ║\n" "Triton Client" "$TRITON_VERSION"
+[[ -n "$TF_PIP_VERSION" ]] && printf "║ %12s | %-61s ║\n" "TensorFlow" "$TF_PIP_VERSION"
+[[ -n "$PT_PIP_VERSION" ]] && printf "║ %12s | %-61s ║\n" "PyTorch" "$PT_PIP_VERSION"
+[[ -n "$NUM_GPUS" ]] && printf "║ %12s | %-61s ║\n" "# GPUs" "$NUM_GPUS"
 
 cat << EOF
-================================================================================
+╚══════════════════════════════════════════════════════════════════════════════╝
 
 EOF
